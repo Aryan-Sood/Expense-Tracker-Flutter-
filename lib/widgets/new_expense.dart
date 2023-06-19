82864b3b29b6f9f8f22e4ecef9aaabb3b11640ff
Aryan <aryansood12@gmail.com>
2023-06-20 01:06:05 +0530

    calender and basic modal sheet

---
+++ lib/widgets/new_expense.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+class newExpense extends StatefulWidget {
+  const newExpense({super.key});
+
+  @override
+  State<newExpense> createState() {
+    return _newExpense();
+  }
+}
+
+// ignore: camel_case_types
+class _newExpense extends State<newExpense> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  void _presentDatePicker() {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+
+    showDatePicker(
+        context: context,
+        initialDate: now,
+        firstDate: firstDate,
+        lastDate: now);
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
+  // var _enteredTitle = '';
+
+  // void _saveTitleInput(String inputValue) {
+  //   _enteredTitle = inputValue;
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(children: [
+        TextField(
+          controller: _titleController,
+          maxLength: 50,
+          keyboardType: TextInputType.text,
+          decoration: const InputDecoration(
+            label: Text('Title'),
+          ),
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: TextField(
+                controller: _amountController,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  prefixText: 'Rs',
+                  label: Text('Amount'),
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: 16,
+            ),
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text('Selected Date'),
+                  IconButton(
+                    onPressed: _presentDatePicker,
+                    icon: const Icon(Icons.calendar_today_rounded),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+        Row(
+          children: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Cancel'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print(_titleController.text);
+                print(_amountController.text);
+              },
+              child: const Text('Save Expense'),
+            ),
+          ],
+        )
+      ]),
+    );
+  }
+}
